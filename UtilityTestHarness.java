2d0bb6af6d4be57fea5764859f012253b24efdbd
Chuck tessein <tessein@gmail.com>
2015-11-06 17:00:12 -0500

    Create UtilityTestHarness.java

---
+++ UtilityTestHarness.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author ctessein
+ *
+ */
+public class UtilityTestHarness {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(Utility.generatePrimefactors(56));
+
+	}
+
+}
