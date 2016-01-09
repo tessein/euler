3c90f895181a225f2d12437054852f1b994857e7
Chuck Tessein <tessein@gmail.com>
2016-01-09 15:55:57 -0500

    Update Euler015.java

--- Euler015.java
+++ Euler015.java
@@ -5,17 +5,22 @@ import java.math.BigInteger;
  * 
  */
 public class Euler015 {
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		final long startTime = System.currentTimeMillis();
 		final BigInteger gridSize = new BigInteger("20");
-		final BigInteger numerator = Utility.factorial(gridSize.add(gridSize));
-		final BigInteger denominator = (Utility.factorial(gridSize).pow(2));
+		final BigInteger numerator = factorial(gridSize.add(gridSize));
+		final BigInteger denominator = (factorial(gridSize).pow(2));
 		System.out.println("Euler015 = " + numerator.divide(denominator)
 		    + " - finished in " + (System.currentTimeMillis() - startTime)
 		    + " millis");
+	}
+	protected static BigInteger factorial(final BigInteger bi) {
+		if (bi.compareTo(new BigInteger("2")) < 0)
+			return BigInteger.ONE;
+		else
+			return bi.multiply(factorial(bi.subtract(BigInteger.ONE)));
 	}
 }
