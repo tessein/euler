1a6d4e123b51dda89ce8b5674a2689ce6b3c47ef
Chuck Tessein <tessein@gmail.com>
2016-01-09 16:11:06 -0500

    Update Euler021.java

--- Euler021.java
+++ Euler021.java
@@ -1,3 +1,4 @@
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
@@ -13,15 +14,30 @@ public class Euler021 {
 		final long startTime = System.currentTimeMillis();
 		long theSum = 0;
 		for (long i = 1; i <= 10000; i++) {
-			TreeSet<Long> firstDivisors = Utility.getDivisors(i);
+			TreeSet<Long> firstDivisors = getDivisors(i);
 			if (firstDivisors.size() > 0) firstDivisors.remove(firstDivisors.last());
-			long firstSum = Utility.getSum(firstDivisors);
-			TreeSet<Long> secondDivisors = Utility.getDivisors(firstSum);
+			long firstSum = getSum(firstDivisors);
+			TreeSet<Long> secondDivisors = getDivisors(firstSum);
 			if (firstDivisors.size() > 0) secondDivisors.remove(secondDivisors.last());
-			long secondSum = Utility.getSum(secondDivisors);
+			long secondSum = getSum(secondDivisors);
 			if (secondSum == i && firstSum != secondSum) 
 				theSum += secondSum;
 		}
 		System.out.println("Euler021 = " + theSum + " - finished in " + (System.currentTimeMillis() - startTime) + " millis" );
+	}
+	protected static long getSum(Set<Long> set) {
+		long theSum = 0;
+		for (long setMember: set)
+			theSum += setMember;
+		return theSum;
+	}
+	protected static TreeSet<Long> getDivisors(long value) {
+		TreeSet<Long> divisors = new TreeSet<Long>();
+		for (long i = 1; i <= Math.sqrt(value); i++) 
+			if (value % i == 0) {
+				divisors.add(i);
+				divisors.add(value/i);
+			}
+		return divisors;
 	}
 }
