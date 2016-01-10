d8b8be4006a6a5ce96d2f6b4a3e798230bdb7b14
Chuck Tessein <tessein@gmail.com>
2016-01-10 09:43:16 -0500

    Update Euler029.java

--- Euler029.java
+++ Euler029.java
@@ -15,20 +15,10 @@ public class Euler029 {
 		final BigInteger low = new BigInteger("2");
 		final BigInteger high = new BigInteger("100");
 		TreeSet<BigInteger> treeset = new TreeSet<BigInteger>();
-		for (BigInteger base = low; base.compareTo(high) <= 0; base = base
-		    .add(BigInteger.ONE)) {
-			System.out.println(base.toString());
-			for (int exponent = 2; exponent <= high.intValue(); exponent++) {
+		for (BigInteger base = low; base.compareTo(high) <= 0; base = base.add(BigInteger.ONE))
+			for (int exponent = 2; exponent <= high.intValue(); exponent++)
 				treeset.add(base.pow(exponent));
-			}
-		}
 		System.out.println("Euler029 = " + treeset.size() + " - finished in "
 		    + (System.currentTimeMillis() - startTime) + " millis.");
-		/*
-		 * TreeSet ts = new TreeSet() (2..100).each { BigInteger base ->
-		 * (2..100).each { BigInteger exp -> ts.add(base.pow(exp)) } } println
-		 * ts.size()
-		 */
 	}
-
 }
