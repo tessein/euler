c9dc4aa327e99f21b43b2b543be28ac6bdaf984d
Chuck Tessein <tessein@gmail.com>
2016-02-02 11:37:12 -0500

    Update Euler038.groovy

--- Euler038.groovy
+++ Euler038.groovy
@@ -1,20 +1,20 @@
 long maxPandigital = 0;
 
-for (int i=2;i < 10000; i++) {
+for (int i=2;i < 1000000; i++) {
   String pandigital = "";
   for (int j = 1; ; j++) {
     pandigital += i * j;
     if (pandigital.length() > 9) {
       break
-    } else if (pandigital.length() == 9) {
+    } 
+    else
       if ((pandigital.length() == 9) 
            && (Long.parseLong(pandigital) % 9 == 0)
            && (pandigital.matches("^(?!.*(.).*\\1)\\d{9}"))
            && (Long.valueOf(pandigital) > maxPandigital)) {
          maxPandigital = Long.valueOf(pandigital);
       }
-      break
-    }
   }
 }
 System.out.println("Maximum pandigital: " + maxPandigital);
+
